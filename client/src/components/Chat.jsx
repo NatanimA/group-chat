3ac88434e0ab2072dafc2d7597591c915e80cd28
NatanimA <Natanimabesha@gmail.com>
2023-03-29 01:07:29 +0300

    Insert Form And Buttons

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, ApolloProvider, gql,useQuery } from '@apollo/client';
-import React from 'react'
+import React,{useState} from 'react'
 import Messages from './Messages';
-import { Container} from 'shards-react'
+import { Button, Col, Container, FormInput, Row} from 'shards-react'
 
 const client = new ApolloClient({
     uri: 'http://localhost:9000/graphql',
@@ -9,10 +9,32 @@ const client = new ApolloClient({
     });
 
 const Chat = () => {
-
+  const [state,setState] = useState({user:'Neo',content:''})
   return (
     <Container>
-        <Messages user='Neo' />
+        <Messages user={state.user} />
+        <Row>
+          <Col xs={2} style={{padding:0}}>
+              <FormInput
+                label="user"
+                value={state.user}
+                onChange={(evt) => setState({...state,user:evt.target.value})}
+              />
+          </Col>
+
+          <Col xs={8} >
+              <FormInput
+                label="content"
+                value={state.content}
+                onChange={(evt) => setState({...state,content:evt.target.value})}
+              />
+          </Col>
+          <Col xs={2} style={{padding:0}}>
+            <Button>
+              Send
+            </Button>
+          </Col>
+        </Row>
     </Container>
   )
 }
