b83554163cb2c10d01eb2b78257b0912bb3d306d
NatanimA <Natanimabesha@gmail.com>
2023-03-29 16:19:04 +0300

    Insert Mutaion Post Message

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,15 +1,52 @@
-import { ApolloClient, InMemoryCache, ApolloProvider, gql,useQuery } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider,split,HttpLink,useMutation } from '@apollo/client';
 import React,{useState} from 'react'
 import Messages from './Messages';
 import { Button, Col, Container, FormInput, Row} from 'shards-react'
+import { POST_MESSAGES } from './queries';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions'
+import { createClient } from 'graphql-ws'
+import { getMainDefinition } from '@apollo/client/utilities';
+
+const wsLink = new GraphQLWsLink(createClient({
+  url:'ws://localhost:9000/graphql'
+}))
+
+const httpLink = new HttpLink({
+  uri:'http://localhost:9000/graphql',
+});
+
+const splitLink = split(
+  ({query}) => {
+    const definition =getMainDefinition(query);
+    return(
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink
+)
+
 
 const client = new ApolloClient({
-    uri: 'http://localhost:9000/graphql',
+    link: splitLink,
     cache: new InMemoryCache(),
     });
 
 const Chat = () => {
   const [state,setState] = useState({user:'Neo',content:''})
+  const [postMessage,{data,loading,error}] = useMutation(POST_MESSAGES)
+
+  const onSend = () => {
+    if(state.content.length < 0) return
+    const {user,content} = state
+    console.log(state)
+    postMessage({variables:{user,content}})
+    setState({
+      ...state,
+      content:''
+    })
+  }
   return (
     <Container>
         <Messages user={state.user} />
@@ -27,10 +64,15 @@ const Chat = () => {
                 label="content"
                 value={state.content}
                 onChange={(evt) => setState({...state,content:evt.target.value})}
+                onKeyUp = {((evt) => {
+                  if(evt.keyCode === 13){
+                    onSend()
+                  }
+                })}
               />
           </Col>
           <Col xs={2} style={{padding:0}}>
-            <Button>
+            <Button onClick={() => onSend()}>
               Send
             </Button>
           </Col>
