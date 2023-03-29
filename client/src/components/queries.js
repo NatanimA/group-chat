384c36406be8dc12d1b1fb45d4f5ea2efbc40b97
NatanimA <Natanimabesha@gmail.com>
2023-03-29 16:20:08 +0300

    Insert Post Message query and Get Notifcations sub

--- client/src/components/queries.js
+++ client/src/components/queries.js
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
 
-export const GET_MESSAGES = () => {
-    return gql `
+export const GET_MESSAGES =
+    gql `
     query {
         messages {
             id
@@ -10,13 +10,25 @@ export const GET_MESSAGES = () => {
         }
     }
     `
-}
 
-export const POST_MESSAGES = (user,content) =>{
-    return gql`
-    mutation(${user},${content}) {
-        id
-        user
-        content
+
+export const POST_MESSAGES = gql `
+    mutation($user: String!,$content:String!){
+        postMessage(user:$user,content:$content){
+            id
+            user
+            content
+        }
     }
-`}
+    `
+
+export const MESSAGE_SUBSCRIPTION = gql `
+    subscription{
+        messageNotification {
+            id
+            user
+            content
+        }
+    }
+`
+
