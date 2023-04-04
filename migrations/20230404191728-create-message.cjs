c11d7e023895dab6138ff75cef6c0444d0a8c5da
NatanimA <Natanimabesha@gmail.com>
2023-04-04 21:47:42 +0300

    Messages Migrations Insertions

---
+++ migrations/20230404191728-create-message.cjs
@@ -0,0 +1,48 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Messages', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4
+      },
+      userId: {
+        type: Sequelize.UUID,
+        references:{
+          model:{
+            tableName:'Users'
+          },
+          key: 'id'
+        },
+        allowNull:false
+      },
+      roomId:{
+        type: Sequelize.UUID,
+        references:{
+          model:{
+            tableName: 'Rooms'
+          },
+          key: 'id'
+        },
+        allowNull:false
+      },
+      content: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Messages');
+  }
+};
