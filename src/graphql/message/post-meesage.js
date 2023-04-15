f1c8804492c5348ba02352e978ff6b1ea60902ee
NatanimA <Natanimabesha@gmail.com>
2023-04-15 13:19:58 +0300

    postMessages Fix

---
+++ src/graphql/message/post-meesage.js
@@ -0,0 +1,15 @@
+const Messages = require('../../../models').Message
+import { PubSub } from 'graphql-subscriptions'
+import { MESSAGE_NOTIFICATION } from '../../../constants'
+
+const pubSub = new PubSub()
+
+const postMessage = async (parent,{user,content,room}) => {
+    try{
+        const newMessage = await Messages({content,userId:user,roomId:room})
+        pubSub.publish(MESSAGE_NOTIFICATION,{messageNotification:newMessage})
+        return newMessage;
+    }catch(error){
+        return error
+    }
+}
