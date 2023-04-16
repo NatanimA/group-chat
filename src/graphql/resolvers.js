6037877fa9bf490913f70b91a7071d907a0ac56b
NatanimA <Natanimabesha@gmail.com>
2023-04-16 18:01:46 +0300

    Modules refactor

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,12 +1,12 @@
-const  {addUser}= require('./user/add-user')
-const { addRoom } = require('./room/add-room')
-const { getRooms} = require('./room/get-rooms')
-const { getRoom } = require('./room/get-room')
-const { getMessages} = require('./message/get-messages')
-const { roomNotification } = require('./room/room-notification')
-import { joinRoom } from './room/join-room';
-import { messageNotification } from './message/message-notification'
-import { postMessages } from './message/post-meesage'
+const { addUser } = require('./user/add-user.js')
+const { addRoom } = require('./room/add-room.js')
+const { getRooms } = require('./room/get-rooms.js')
+const { getRoom } = require('./room/get-room.js')
+const { getMessages } = require('./message/get-messages.js')
+const { roomNotification } = require("./room/room-notification.js")
+const { joinRoom } = require('./room/join-room.js')
+const { messageNotification } = require('./message/message-notification.js')
+const { postMessages } = require('./message/post-meesage.js')
 
 
 const resolvers = {
