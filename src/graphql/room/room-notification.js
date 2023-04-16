e6b52d5d666232c934e7c15881501c3bcad839f5
NatanimA <Natanimabesha@gmail.com>
2023-04-16 18:05:29 +0300

    room notifications module refactor

--- src/graphql/room/room-notification.js
+++ src/graphql/room/room-notification.js
@@ -1,13 +1,13 @@
 import { PubSub,withFilter } from "graphql-subscriptions"
-import { ROOM_NOTIFICATION } from "../../../constants"
+const {ROOM_NOTIFICATION } = require("../../../constants")
 
 const pubSub = new PubSub()
 
 
-const roomNotification = {
+export const roomNotification = {
     subscribe: withFilter(() => pubSub.asyncIterator([ROOM_NOTIFICATION]),(payload,variables) => {
                 return(payload.roomNotification.id === variables.roomId)
             })
 }
 
-module.exports = { roomNotification }
+
--- src/graphql/user/add-user.js
+++ src/graphql/user/add-user.js
@@ -1,10 +1,11 @@
-const Users = require('../../../models').User
+const Users = require("../../../models").User
 
 
-const addUser = async (parent,arg) => {
+export const addUser = async (parent,arg) => {
             const {name,email} = arg
             const newUser = await Users.create({name,email})
             return newUser;
         }
 
-module.exports = {addUser}
+
+
