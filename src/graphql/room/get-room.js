d58769a3d2fc6188cfc49492f1f0f4df378d5a33
NatanimA <Natanimabesha@gmail.com>
2023-04-16 18:04:41 +0300

    get room module refactor

--- src/graphql/room/get-room.js
+++ src/graphql/room/get-room.js
@@ -1,6 +1,6 @@
 const Rooms = require('../../../models').Room
 
-const getRoom = async (parent,arg) => {
+export const getRoom = async (parent,arg) => {
             const {id} = arg
             return Rooms.findOne({
                 where:{
@@ -9,4 +9,4 @@ const getRoom = async (parent,arg) => {
             })
         }
 
-module.exports = { getRoom}
+
