9edcf3504cc0f6b922aebf310410437d81ea532c
NatanimA <Natanimabesha@gmail.com>
2023-04-20 16:18:13 +0300

    Update Index Db container

--- constants/index.js
+++ constants/index.js
@@ -1,2 +1,9 @@
-export const ROOM_NOTIFICATION = 'ROOM_NOTIFICATION'
-export const MESSAGE_NOTIFICATION = 'MESSAGE_NOTIFICATION'
+const ROOM_NOTIFICATION = 'ROOM_NOTIFICATION'
+const MESSAGE_NOTIFICATION = 'MESSAGE_NOTIFICATION'
+const Users = require('../models').User
+const Messages = require('../models').Message
+const Rooms= require('../models').Room
+const UserRooms= require('../models').UserRoom
+
+
+module.exports ={ROOM_NOTIFICATION,MESSAGE_NOTIFICATION,Users,Messages,Rooms,UserRooms}
