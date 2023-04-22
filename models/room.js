a8417be9156012d3e3c98cc8428e278da0981cc8
NatanimA <Natanimabesha@gmail.com>
2023-04-22 15:46:32 +0300

    Room Models Refactor

--- models/room.js
+++ models/room.js
@@ -1,34 +1,33 @@
 'use strict';
 const {
-  Model
+Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Room extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      Room.hasMany(models.Message,{
-        foreignKey:'roomId',
-        as: 'messages'
-      })
+class Room extends Model {
+/**
+* Helper method for defining associations.
+* This method is not a part of Sequelize lifecycle.
+* The models/index file will call this method automatically.
+*/
+static associate(models) {
+Room.hasMany(models.Message,{
+foreignKey:'roomId'
+})
 
-      Room.hasMany(models.User,{
-        through:'RoomUsers',
-        foreignKey: 'roomId',
-        as: 'users'
-      })
-    }
-  }
-  Room.init({
-    name: DataTypes.STRING,
-    messages: DataTypes.STRING,
-    users: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Room',
-  });
-  return Room;
+Room.belongsToMany(models.User,{
+    through:'RoomUsers',
+    foreignKey: 'roomId'
+  })
+}
+
+}
+Room.init({
+name: DataTypes.STRING,
+messages: DataTypes.STRING,
+users: DataTypes.STRING
+}, {
+sequelize,
+modelName: 'Room',
+});
+return Room;
 };
