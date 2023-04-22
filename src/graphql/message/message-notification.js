74d94928883835fba2d66299f0d6bb7fd10bfb44
NatanimA <Natanimabesha@gmail.com>
2023-04-22 15:48:57 +0300

    Message Notifications Context Insertions

--- src/graphql/message/message-notification.js
+++ src/graphql/message/message-notification.js
@@ -1,14 +1,16 @@
-import { PubSub,withFilter } from "graphql-subscriptions"
-const { MESSAGE_NOTIFICATION } = require("../../../constants/index.js")
+const { PubSub, withFilter } = require('graphql-subscriptions');
+const { MESSAGE_NOTIFICATION } = require('../../../constants/index.js');
 
 const pubSub = new PubSub()
 
 
-export const messageNotification = {
+const messageNotification = {
     subscribe: withFilter(() => pubSub.asyncIterator([MESSAGE_NOTIFICATION]),(payload,variables) => {
                 return(variables.room.includes(payload.messageNotification.room.id))
             })
 }
 
+
+module.exports = {messageNotification}
 
 
