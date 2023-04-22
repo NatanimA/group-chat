6b7efc1bb16300d9d30b93f385fdf4a1190d9130
NatanimA <Natanimabesha@gmail.com>
2023-04-22 15:47:09 +0300

    RoomUser Model Refactor

--- models/roomuser.js
+++ models/roomuser.js
@@ -1,48 +1,49 @@
 'use strict';
 const {
-  Model
+Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class RoomUser extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      RoomUser.belongsTo(models.User,{
-        foreignKey:'userId'
-      })
+class RoomUser extends Model {
+/**
+* Helper method for defining associations.
+* This method is not a part of Sequelize lifecycle.
+* The models/index file will call this method automatically.
+*/
+static associate(models) {
+RoomUser.belongsTo(models.User,{
+foreignKey:'userId'
+})
 
-      RoomUser.belongsTo(models.Room,{
-        foreignKey:'roomId'
-      })
-    }
-  }
-  RoomUser.init({
-   userId: {
-        type: DataTypes.UUID,
-        references:{
-          model:{
-            tableName:'Users'
-          },
-          key: 'id'
-        },
-        allowNull:false
-      },
-      roomId:{
-        type: DataTypes.UUID,
-        references:{
-          model:{
-            tableName: 'Rooms'
-          },
-          key: 'id'
-        },
-        allowNull:false
-      },
-  }, {
-    sequelize,
-    modelName: 'RoomUser',
-  });
-  return RoomUser;
+RoomUser.belongsTo(models.Room,{
+    foreignKey:'roomId'
+  })
+}
+
+}
+RoomUser.init({
+userId: {
+type: DataTypes.UUID,
+references:{
+model:{
+tableName:'Users'
+},
+key: 'id'
+},
+allowNull:false
+},
+roomId:{
+type: DataTypes.UUID,
+references:{
+model:{
+tableName: 'Rooms'
+},
+key: 'id'
+},
+allowNull:false
+},
+}, {
+sequelize,
+modelName: 'RoomUser',
+});
+return RoomUser;
 };
