18a75a275af1bbc491db65f47a7ee0d115ff33fe
NatanimA <Natanimabesha@gmail.com>
2023-04-22 15:44:00 +0300

    Constants Exports Refactors

--- constants/index.js
+++ constants/index.js
@@ -1,9 +1,4 @@
 const ROOM_NOTIFICATION = 'ROOM_NOTIFICATION'
 const MESSAGE_NOTIFICATION = 'MESSAGE_NOTIFICATION'
-const Users = require('../models').User
-const Messages = require('../models').Message
-const Rooms= require('../models').Room
-const UserRooms= require('../models').UserRoom
 
-
-module.exports ={ROOM_NOTIFICATION,MESSAGE_NOTIFICATION,Users,Messages,Rooms,UserRooms}
+module.exports = {ROOM_NOTIFICATION,MESSAGE_NOTIFICATION}
