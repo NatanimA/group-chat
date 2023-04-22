6f7ad6a3ec0fb49ef0d916698d2a658b9e56a20a
NatanimA <Natanimabesha@gmail.com>
2023-04-22 15:51:03 +0300

    Room Notifications Context Insertions

--- src/graphql/room/room-notification.js
+++ src/graphql/room/room-notification.js
@@ -1,13 +1,15 @@
-import { PubSub,withFilter } from "graphql-subscriptions"
-const {ROOM_NOTIFICATION } = require("../../../constants/index.js")
+const { PubSub, withFilter } = require('graphql-subscriptions');
+const { ROOM_NOTIFICATION } = require('../../../constants/index.js');
 
 const pubSub = new PubSub()
 
 
-export const roomNotification = {
+const roomNotification = {
     subscribe: withFilter(() => pubSub.asyncIterator([ROOM_NOTIFICATION]),(payload,variables) => {
                 return(payload.roomNotification.id === variables.roomId)
             })
 }
+
+module.exports ={roomNotification}
 
 
