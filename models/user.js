ac035d8c5fd425356fc563ceaa364ebb18c480f9
NatanimA <Natanimabesha@gmail.com>
2023-04-22 15:47:25 +0300

    User Models Refactor

--- models/user.js
+++ models/user.js
@@ -1,35 +1,34 @@
 'use strict';
 const {
-  Model
+Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      User.hasMany(models.Message,{
-        foreignKey:'userId',
-        as: 'events'
-      },)
+class User extends Model {
+/**
+* Helper method for defining associations.
+* This method is not a part of Sequelize lifecycle.
+* The models/index file will call this method automatically.
+*/
+static associate(models) {
+// define association here
+User.hasMany(models.Message,{
+foreignKey:'userId'
+},)
 
-      User.belongsToMany(models.Room,{
-        through:'RoomUsers',
-        foreignKey:'userId',
-        as: 'rooms'
-      })
-    }
-  }
-  User.init({
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
-  return User;
+User.belongsToMany(models.Room,{
+    through:'RoomUsers',
+    foreignKey:'userId'
+  })
+}
+
+}
+User.init({
+name: DataTypes.STRING,
+email: DataTypes.STRING,
+password: DataTypes.STRING
+}, {
+sequelize,
+modelName: 'User',
+});
+return User;
 };
