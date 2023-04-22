a9b4e477090be54474d7cce214eae42bdec21709
NatanimA <Natanimabesha@gmail.com>
2023-04-22 15:44:36 +0300

    Message Models refactor

--- models/message.js
+++ models/message.js
@@ -1,52 +1,52 @@
 'use strict';
 const {
-  Model
+Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Message extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      Message.belongsTo(models.User,{
-        foreignKey:'userId',
-        as:'user'
-      })
+class Message extends Model {
+/**
+* Helper method for defining associations.
+* This method is not a part of Sequelize lifecycle.
+* The models/index file will call this method automatically.
+*/
+static associate(models) {
+// define association here
+Message.belongsTo(models.User,{
+foreignKey:'userId',
+as:'user'
+})
 
-      Message.belongsTo(models.Room,{
-        foreignKey:'roomId',
-        as:'room'
-      })
-    }
-  }
-  Message.init({
-    userId: {
-        type: DataTypes.UUID,
-        references:{
-          model:{
-            tableName:'Users'
-          },
-          key: 'id'
-        },
-        allowNull:false
-      },
-      roomId:{
-        type: DataTypes.UUID,
-        references:{
-          model:{
-            tableName: 'Rooms'
-          },
-          key: 'id'
-        },
-        allowNull:false
-      },
-    content: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Message',
-  });
-  return Message;
+Message.belongsTo(models.Room,{
+    foreignKey:'roomId'
+  })
+}
+
+}
+Message.init({
+userId: {
+type: DataTypes.UUID,
+references:{
+model:{
+tableName:'Users'
+},
+key: 'id'
+},
+allowNull:false
+},
+roomId:{
+type: DataTypes.UUID,
+references:{
+model:{
+tableName: 'Rooms'
+},
+key: 'id'
+},
+allowNull:false
+},
+content: DataTypes.STRING
+}, {
+sequelize,
+modelName: 'Message',
+});
+return Message;
 };
