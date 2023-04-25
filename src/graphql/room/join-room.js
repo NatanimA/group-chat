b5a1f9892614b310fd691b773db294d26f590891
NatanimA <Natanimabesha@gmail.com>
2023-04-25 18:21:11 +0300

    Join Room Table Assosiations Fix

--- src/graphql/room/join-room.js
+++ src/graphql/room/join-room.js
@@ -6,14 +6,24 @@ const pubSub = new PubSub()
 
 const joinRoom = async(_,arg,{models}) => {
     const {userId,roomId} = arg
-    const {Room,RoomUser} = models
+    const {Room,RoomUser,User,Message} = models
     try {
         await RoomUser.create({roomId,userId})
         const room = await Room.findOne({
         raw:true,
         where:{
             id:roomId
-        }
+        },
+        include:[
+            {
+                model:User,
+                as:"users"
+            },
+            {
+                model:Message,
+                as:"messages"
+            }
+        ]
          })
         pubSub.publish(ROOM_NOTIFICATION,{roomNotification:room})
         return room;
