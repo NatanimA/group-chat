896502d25d01881dd760b478578e89cd59b98626
NatanimA <Natanimabesha@gmail.com>
2023-04-27 22:06:34 +0300

    Post Messages Update

--- src/graphql/message/post-meesage.js
+++ src/graphql/message/post-meesage.js
@@ -4,9 +4,22 @@ const { MESSAGE_NOTIFICATION } = require('../../../constants/index.js');
 const pubSub = new PubSub()
 
 const postMessages = async (_,{user,content,room},{models}) => {
-    const {Message} = models
+    const {Message,User,Room} = models
     try{
-        const newMessage = await Message({content,userId:user,roomId:room})
+        const newMessage = await Message.create({content,userId:user,roomId:room},{
+            returning:true,
+            include:[
+                {
+                model:User,
+                as:"user"
+                },
+                {
+                    model:Room,
+                    as:"room"
+                }
+        ]
+        })
+        console.log("Message: ",newMessage)
         pubSub.publish(MESSAGE_NOTIFICATION,{messageNotification:newMessage})
         return newMessage;
     }catch(error){
