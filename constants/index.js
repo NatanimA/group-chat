449c62633c642de73bb68fa7131400bbeaddf55e
NatanimA <Natanimabesha@gmail.com>
2023-04-30 20:39:25 +0300

    Pub Sub Migrations Same Instance Usage

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,7 @@
+const { PubSub } = require("graphql-subscriptions")
+
 const ROOM_NOTIFICATION = 'ROOM_NOTIFICATION'
 const MESSAGE_NOTIFICATION = 'MESSAGE_NOTIFICATION'
+const pubSub = new PubSub()
 
-module.exports = {ROOM_NOTIFICATION,MESSAGE_NOTIFICATION}
+module.exports = {ROOM_NOTIFICATION,MESSAGE_NOTIFICATION,pubSub}
