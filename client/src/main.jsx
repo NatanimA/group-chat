4d1acc801d1c3dde79207e8cc4282d337c259089
NatanimA <Natanimabesha@gmail.com>
2023-05-01 14:08:55 +0300

    Main Component Router Insertions

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,9 +4,16 @@ import App from './App'
 import './index.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "shards-ui/dist/css/shards.min.css"
+import {BrowserRouter,Routes , Route} from "react-router-dom"
+import Auth from './components/Auth/Auth';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+          <Route path='/chat'  element={<App />} />
+          <Route path='/auth/login' element={<Auth />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
