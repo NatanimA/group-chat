e04eb65234753bd9154f5413d4ddf3fccc9c62be
NatanimA <Natanimabesha@gmail.com>
2023-05-02 16:14:54 +0300

    main Google OAuth Provider Extenstions

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,14 +6,22 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "shards-ui/dist/css/shards.min.css"
 import {BrowserRouter,Routes , Route} from "react-router-dom"
 import Auth from './components/Auth/Auth';
+import ProtectedRoute from "./components/ProtectedRoute"
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-          <Route path='/chat'  element={<App />} />
-          <Route path='/auth/login' element={<Auth />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
+  <GoogleOAuthProvider clientId="320120764050-l4lf7hpo8oeqvevk23iv0hiv3hsv440j.apps.googleusercontent.com">
+      <React.StrictMode>
+        <BrowserRouter>
+          <Routes>
+              <Route path='/'  element={
+              <ProtectedRoute>
+                <App />
+              </ProtectedRoute>} />
+              <Route path='/auth/login' element={<Auth />} />
+          </Routes>
+        </BrowserRouter>
+      </React.StrictMode>
+  </GoogleOAuthProvider>
+  ,
 )
