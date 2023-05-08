c9011fbd94726978885f407f7fcc35cdb60f33d7
NatanimA <Natanimabesha@gmail.com>
2023-05-08 16:05:32 +0300

    Host Vite Fix change to LH

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,20 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 import { defineConfig } from 'vite'
+import dns from 'dns'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+dns.setDefaultResultOrder('verbatim')
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: 'localhost',
+    port: 3000
+  }
 })
